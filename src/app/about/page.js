7df5a10f9feb3ba6ab45700a1f348e652f9bae48
sowmik-sec <sowmik.sec@gmail.com>
2025-05-05 15:05:24 +0600

    Use metadata for better SEO and Import alias

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "About page",
+  description: "This is about page",
+};
+function AboutPage() {
+  return (
+    <div>
+      <h2>This is about page</h2>
+    </div>
+  );
+}
+
+export default AboutPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Home Page",
+  description: "This is home page",
+};
 function HomePage() {
   return (
     <div>
