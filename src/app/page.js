e27d9371f1e7966593947aac5467b26d0219dada
sowmik-sec <sowmik.sec@gmail.com>
2025-05-05 15:33:49 +0600

    Explain SSG by Next-level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,20 @@ export const metadata = {
   description: "This is home page",
 };
 async function HomePage() {
-  const res = await fetch("http://localhost:5000/shoes");
+  const res = await fetch("http://localhost:5000/shoes", {
+    cache: "force-cache",
+  });
   const shoes = await res.json();
   return (
     <div>
       <h1 className="text-4xl text-center">Hello world</h1>
 
-      <div>
+      <div className="grid grid-cols-3 gap-6">
         {shoes.map((shoe) => (
           <div key={shoe.id}>
             <p>{shoe.title}</p>
+            <p>{shoe.price}</p>
+            <p>{shoe.description}</p>
           </div>
         ))}
       </div>
