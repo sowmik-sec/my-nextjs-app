85739e9fc34c79e76f68a3eea145fe8333db16e7
sowmik-sec <sowmik.sec@gmail.com>
2025-05-05 15:29:34 +0600

    Set-up Json Server for Next Level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,20 @@ export const metadata = {
   title: "Home Page",
   description: "This is home page",
 };
-function HomePage() {
+async function HomePage() {
+  const res = await fetch("http://localhost:5000/shoes");
+  const shoes = await res.json();
   return (
     <div>
       <h1 className="text-4xl text-center">Hello world</h1>
+
+      <div>
+        {shoes.map((shoe) => (
+          <div key={shoe.id}>
+            <p>{shoe.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
